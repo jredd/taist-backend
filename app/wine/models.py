164d3ff979827e5c4faa5ab8c1b92de20283c707
jredd <jredd46@gmail.com>
2020-02-15 12:51:06 -0600

    added some base models

--- app/wine/models.py
+++ app/wine/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
+
 # Create your models here.
+class Winery(models.Model):
+
+    name = models.CharField(max_length=50, blank=False, unique=True)
+    date_added = models.DateField(auto_now_add=True)
+    location = models.CharField(max_length=500, blank=False, unique=True)
+
+
+class WineAlias(models.Model):
+
+    name = models.CharField(max_length=50, blank=False, unique=True)
+    site_associated_with = models.URLField()
+    date_added = models.DateField(auto_now_add=True)
+
+
+class Wine(models.Model):
+
+    winery = models.ForeignKey(Winery, on_delete=models.PROTECT)
+    name = models.CharField(max_length=50, blank=False,)
+    vintage = models.DateField(blank=False)
+    wine_alias = models.ForeignKey(WineAlias, on_delete=models.CASCADE)
+
+
+class Review(models.Model):
+
+    review = models.CharField(max_length=500)
+    vintage = models.DateField(blank=True)
+
+
