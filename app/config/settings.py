95e75215a9a7f39b1183dc5160582c15c13df1d2
jredd <jredd46@gmail.com>
2020-02-17 19:51:48 -0600

    Got the docker images up and running and docker compose works

--- app/config/settings.py
+++ app/config/settings.py
@@ -122,5 +122,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
+MEDIA_URL = '/media/'
